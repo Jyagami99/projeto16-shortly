3959de1442bff2081fbd11f9261bdf7334a1ac47
João Marcelo Simão de Castro <jm.castro@usp.br>
2022-08-08 16:52:42 -0300

    fix: deploy on heroku

--- src/db/database.js
+++ src/db/database.js
@@ -5,15 +5,10 @@ dotenv.config();
 
 const { Pool } = pg;
 const databaseConfig = {
-  host: "localhost",
-  port: 5432,
-  user: "postgres",
-  password: "pass@1010",
-  database: "shortly",
-  // connectionString: process.env.DATABASE_URL,
-  // ssl: {
-  //   rejectUnauthorized: false,
-  // },
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 };
 
 const db = new Pool(databaseConfig);
